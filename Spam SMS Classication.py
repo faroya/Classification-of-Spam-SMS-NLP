ad44d4ce988924dd653bb8afdad9d2cb794fb435
Farouk YAHAYA <faroya2011@gmail.com>
2023-06-02 10:36:00 +0200

    Add files via upload

---
+++ Spam SMS Classication.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Importing essential libraries
+import numpy as np
+import pandas as pd
+# Importing essential libraries for visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Importing essential libraries for performing NLP
+import nltk
+import re
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+
+# Loading the dataset
+df = pd.read_csv('Spam SMS Collection', sep='\t', names=['label', 'message'])
+df.describe(include='object')
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+
+# Countplot for Spam vs. Ham as imbalanced dataset
+plt.figure(figsize=(8,8))
+g = sns.countplot(x='label', data=df)
+p = plt.title('Countplot for Spam vs. Ham as imbalanced dataset')
+p = plt.xlabel('Is SMS Spam?')
+p = plt.ylabel('Count')
+
+# Handling imbalanced dataset using Oversampling
+only_spam = df[df['label']==1]
+print('Number of Spam records: {}'.format(only_spam.shape[0]))
+print('Number of Ham records: {}'.format(df.shape[0]-only_spam.shape[0]))
+
+
+count = int((df.shape[0]-only_spam.shape[0])/only_spam.shape[0])
+for i in range(0, count-1):
+  df = pd.concat([df, only_spam])
+
+# Countplot for Spam vs. Ham as balanced dataset
+plt.figure(figsize=(8,8))
+g = sns.countplot(x='label', data=df)
+p = plt.title('Countplot for Spam vs. Ham as balanced dataset')
+p = plt.xlabel('Is SMS Spam?')
+p = plt.ylabel('Count')
+
+ 
+
+# Creating new feature word_count
+df['word_count'] = df['message'].apply(lambda x: len(x.split()))
+plt.figure(figsize=(12, 6))
+
+# 1-row, 2-column, go to the first subplot
+plt.subplot(1, 2, 1)
+g = sns.distplot(a=df[df['label']==0].word_count)
+p = plt.title('Distribution of word_count for Ham messages')
+
+# 1-row, 2-column, go to the second subplot
+plt.subplot(1, 2, 2)
+g = sns.distplot(a=df[df['label']==1].word_count, color='red')
+p = plt.title('Distribution of word_count for Spam messages')
+
+plt.tight_layout()
+plt.show()
+
+# Creating feature contains_currency_symbol
+def currency(x):
+  currency_symbols = ['€', '$', '¥', '£', '₹']
+  for i in currency_symbols:
+    if i in x:
+      return 1
+  return 0
+
+df['contains_currency_symbol'] = df['message'].apply(currency)
+# Countplot for contains_currency_symbol
+plt.figure(figsize=(8,8))
+g = sns.countplot(x='contains_currency_symbol', data=df, hue='label')
+p = plt.title('Countplot for contain_currency')
+p = plt.xlabel('Does SMS contain currency symbol?')
+p = plt.ylabel('Count')
+p = plt.legend(labels=['Ham', 'Spam'], loc=9)
+
+# Creating feature contains_number
+def numbers(x):
+  for i in x:
+    if ord(i)>=48 and ord(i)<=57:
+      return 1
+  return 0
+
+df['contains_number'] = df['message'].apply(numbers)
+
+# Countplot for contains_number
+plt.figure(figsize=(8,8))
+g = sns.countplot(x='contains_number', data=df, hue='label')
+p = plt.title('Countplot for contain_numbers')
+p = plt.xlabel('Does SMS contain number?')
+p = plt.ylabel('Count')
+p = plt.legend(labels=['Ham', 'Spam'], loc=9)
+
+# Cleaning the messages
+corpus = []
+wnl = WordNetLemmatizer()
+
+for sms_string in list(df.message):
+
+  # Cleaning special character from the sms
+  message = re.sub(pattern='[^a-zA-Z]', repl=' ', string=sms_string)
+
+  # Converting the entire sms into lower case
+  message = message.lower()
+
+  # Tokenizing the sms by words
+  words = message.split()
+
+  # Removing the stop words
+  filtered_words = [word for word in words if word not in set(stopwords.words('english'))]
+
+  # Lemmatizing the words
+  lemmatized_words = [wnl.lemmatize(word) for word in filtered_words]
+
+  # Joining the lemmatized words
+  message = ' '.join(lemmatized_words)
+
+  # Building a corpus of messages
+  corpus.append(message)
+
+corpus[0:3]
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(max_features=500)
+vectors = tfidf.fit_transform(corpus).toarray()
+feature_names = tfidf.get_feature_names()
+
+# Extracting independent and dependent variables from the dataset
+X = pd.DataFrame(vectors, columns=feature_names)
+y = df['label']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import MultinomialNB
+mnb = MultinomialNB()
+cv = cross_val_score(mnb, X, y, scoring='f1', cv=10)
+print('--- Average F1-Score for MNB model: {} ---'.format(round(cv.mean(), 3)))
+print('Standard Deviation: {}'.format(round(cv.std(), 3)))
+
+# Classification report for MNB model
+mnb = MultinomialNB()
+mnb.fit(X_train, y_train)
+y_pred = mnb.predict(X_test)
+
+print('--- Classification report for MNB model ---')
+print(classification_report(y_test, y_pred))
+
+# Confusion matrix of MNB model
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(8,5))
+axis_labels = ['ham', 'spam']
+g = sns.heatmap(data=cm, annot=True, cmap="Blues", xticklabels=axis_labels, yticklabels=axis_labels, fmt='g', cbar_kws={"shrink": 0.5})
+p = plt.xlabel('Actual values')
+p = plt.ylabel('Predicted values')
+p = plt.title('--- Confusion Matrix for Multinomial Naive Bayes model ---')
+
+# Fitting Random Forest to the Training set
+from sklearn.tree import DecisionTreeClassifier
+dt = DecisionTreeClassifier()
+cv = cross_val_score(dt, X, y, scoring='f1', cv=10)
+print('--- Average F1-Score for Decision Tree model: {} ---'.format(round(cv.mean(), 3)))
+print('Standard Deviation: {}'.format(round(cv.std(), 3)))
+
+
+# Classification report for Decision Tree model
+dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
+y_pred = dt.predict(X_test)
+
+print('--- Classification report for Decision Tree model ---')
+print(classification_report(y_test, y_pred))
+
+# Confusion matrix of Decision Tree model
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(8,5))
+axis_labels = ['ham', 'spam']
+g = sns.heatmap(data=cm, annot=True, cmap="Blues", xticklabels=axis_labels, yticklabels=axis_labels, fmt='g', cbar_kws={"shrink": 0.5})
+p = plt.xlabel('Actual values')
+p = plt.ylabel('Predicted values')
+p = plt.title('--- Confusion Matrix for Decision Tree model ---')
+
+# Fitting Random Forest to the Training set
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators=10)
+cv = cross_val_score(rf, X, y, scoring='f1', cv=10)
+print('--- Average F1-Score for Random Forest model: {} ---'.format(round(cv.mean(), 3)))
+print('Standard Deviation: {}'.format(round(cv.std(), 3)))
+
+
+# Classification report for Random Forest model
+rf = RandomForestClassifier(n_estimators=20)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+print('--- Classification report for Random Forest model ---')
+print(classification_report(y_test, y_pred))
+
+
+# Confusion matrix of Random Forest model
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(8,5))
+axis_labels = ['ham', 'spam']
+g = sns.heatmap(data=cm, annot=True, cmap="Blues", xticklabels=axis_labels, yticklabels=axis_labels, fmt='g', cbar_kws={"shrink": 0.5})
+p = plt.xlabel('Actual values')
+p = plt.ylabel('Predicted values')
+p = plt.title('--- Confusion Matrix for Random Forest model ---')
+
+
+# Fitting Decision Tree and MNB to VotingClassifier
+from sklearn.ensemble import VotingClassifier
+vc = VotingClassifier([('decision_tree', dt), ('m_naive_bayes', mnb)], weights=[2,1])
+cv = cross_val_score(vc, X, y, cv=10, scoring='f1')
+
+print('--- Average F1-Score for VotingClassifier model: {} ---'.format(round(cv.mean(), 3)))
+print('Standard Deviation: {}'.format(round(cv.std(), 3)))
+ 
+
+def predict_spam(sample_message):
+  sample_message = re.sub(pattern='[^a-zA-Z]',repl=' ', string = sample_message)
+  sample_message = sample_message.lower()
+  sample_message_words = sample_message.split()
+  sample_message_words = [word for word in sample_message_words if not word in set(stopwords.words('english'))]
+  final_message = [wnl.lemmatize(word) for word in sample_message_words]
+  final_message = ' '.join(final_message)
+
+  temp = tfidf.transform([final_message]).toarray()
+  return rf.predict(temp)
+
+
+# Prediction 1 - Lottery text message
+sample_message = 'IMPORTANT - You could be entitled up to £3,160 in compensation from mis-sold PPI on a credit card or loan. Please reply PPI for info or STOP to opt out.'
+
+if predict_spam(sample_message):
+  print('Gotcha! This is a SPAM message.')
+else:
+  print('This is a HAM (normal) message.')
+
+
+
+# Prediction 2 - Casual text chat
+sample_message = 'Came to think of it. I have never got a spam message before.'
+
+if predict_spam(sample_message):
+  print('Gotcha! This is a SPAM message.')
+else:
+  print('This is a HAM (normal) message.')
+
+ 
+
+# Prediction 3 - Transaction confirmation text message
+sample_message = 'Sam, your rent payment for Jan 19 has been received. $1,300 will be drafted from your Wells Fargo Account ******0000 within 24-48 business hours. Thank you!'
+
+if predict_spam(sample_message):
+  print('Gotcha! This is a SPAM message.')
+else:
+  print('This is a HAM (normal) message.')
+ 
+
+# Predicting values 4 - Feedback message
+sample_message = 'Tammy, thanks for choosing Carl’s Car Wash for your express polish. We would love to hear your thoughts on the service. Feel free to text back with any feedback. Safe driving!'
+
+if predict_spam(sample_message):
+  print('Gotcha! This is a SPAM message.')
+else:
+  print('This is a HAM (normal) message.')
+
